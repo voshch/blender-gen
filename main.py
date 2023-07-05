7645becffc3ccf8ef10305cc9792727a2dfc854f
Vova Shcherbyna <volodymyr.shcherbyna@campus.tu-berlin.de>
2023-07-05 14:10:49 +0200

    parametrization

--- main.py
+++ main.py
@@ -6,10 +6,12 @@ import subprocess
 import grequests as requests
 import socket
 
-sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), "src"))
-import util
+sys.path.append(os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), "src"))
 
-log = util.Log()
+from util import Target, Mode, Log
+
+log = Log()
 
 
 def configure(python_cmd, mode):
@@ -26,13 +28,13 @@ def render():
     log.print(f"RUNNING RENDER STEP:\n")
     proc = subprocess.Popen(["blender", "--background", "--python",
                             "./src/render.py"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-    
+
     proc.wait()
-    
+
     if os.path.isfile("/data/intermediate/render/render.lock"):
         os.remove("/data/intermediate/render/render.lock")
         return 0
-    
+
     return 1
 
 
@@ -41,6 +43,7 @@ def merge(python_cmd, mode):
     proc = subprocess.Popen([python_cmd, "./src/merge.py", "--mode_internal",
                             mode, *sys.argv[1:]], stdout=log.stdout, stderr=log.stderr)
     return proc.wait()
+
 
 def postfx(python_cmd, mode):
     log.print(f"RUNNING POSTFX STEP:\n")
@@ -53,37 +56,42 @@ def run(taskid, target, endpoint, mode):
     python_cmd = os.path.realpath(sys.executable)
 
     log.print(f"!!RUNNING BLENDER-GEN IN MODE {mode}\n")
-    if target in ["all", "configure"]:
-        status = configure(python_cmd, mode)
-        if status != 0:
-            raise RuntimeError(
-                f"configure step failed with {status}")
 
-    if target in ["all", "render"]:
-        status = render()
-        if status != 0:
-            raise RuntimeError(
-                f"render step failed with {status}")
+    match target:
+        case Target.Configure:
+            status = configure(python_cmd, mode)
+            if status != 0:
+                raise RuntimeError(
+                    f"configure step failed with {status}")
 
-    if target in ["all", "merge"]:
-        status = merge(python_cmd, mode)
-        if status != 0:
-            raise RuntimeError(
-                f"merge step failed with {status}")
-        
-    if target in ["all", "postfx"]:
-        status = postfx(python_cmd, mode)
-        if status != 0:
-            raise RuntimeError(
-                f"postfx step failed with {status}")
+        case Target.Render:
+            status = render()
+            if status != 0:
+                raise RuntimeError(
+                    f"render step failed with {status}")
+
+        case Target.Merge:
+            status = merge(python_cmd, mode)
+            if status != 0:
+                raise RuntimeError(
+                    f"merge step failed with {status}")
+
+        case Target.PostFX:
+            status = postfx(python_cmd, mode)
+            if status != 0:
+                raise RuntimeError(
+                    f"postfx step failed with {status}")
+
+        case _:
+            raise ValueError(f"unknown target {target}")
 
 
 @click.command(context_settings=dict(
     ignore_unknown_options=True,
     allow_extra_args=True,
 ))
-@click.option("--mode", default="all", help="all|train|val create training or validation images")
-@click.option("--target", type=click.Choice(["all", "configure", "render", "merge", "postfx"]), default="all")
+@click.option("--mode", type=click.Choice(["all", Mode.Train, Mode.Val]), default="all", help="all|train|val create training or validation images")
+@click.option("--target", default="all")
 @click.option("--endpoint", default=None, help="http endpoint for sending current progress")
 @click.option("--taskID", default="", help="task ID")
 @click.option("--output", type=click.Choice(["shell", "file"]), default="shell", help="output to stdout or to /data/output/log.txt")
@@ -101,14 +109,16 @@ def main(mode, target, endpoint, taskid, output):
         open("/data/log/stderr.txt", "w").close()
         log.stderr = open("/data/log/stderr.txt", "a")
 
-    try:
-        if mode in ["train", "all"]:
-            run(taskid, target, endpoint, "train")
-            log.print("\n")
+    target = [Target.Configure, Target.Render, Target.Merge,
+              Target.PostFX] if target == "all" else target.split(",")
+    
+    mode = [Mode.Train, Mode.Val] if mode == "all" else [mode]
 
-        if mode in ["val", "all"]:
-            run(taskid, target, endpoint, "val")
-            log.print("\n")
+    try:
+        for current_target in target:
+            for current_mode in mode:
+                run(taskid, current_target, endpoint, current_mode)
+                log.print("\n")
 
     except RuntimeError as e:
         if endpoint != None:
